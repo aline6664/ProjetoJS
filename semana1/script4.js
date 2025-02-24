47f4afd4a7784ef579063d3237146a6b3ca087dd
aline6664 <gley.line@gmail.com>
2025-02-24 18:14:42 -0300

    UPDATE semana 1 ex 4, semana 2 ex 3

--- semana1/script4.js
+++ semana1/script4.js
@@ -1,20 +1,32 @@
-let dado = prompt("Insira a entrada de um dado qualquer: ");
+// let dado = prompt("Insira a entrada de um dado qualquer: ");
 
-if(confirm("Deseja verificar o tipo do dado informado?")) {
-    // numero
-    if ((!isNaN(dado) && dado.trim() !== "")) {
-        let dadoNumero = Number(dado);
-        document.getElementById("resultado").innerHTML = "O tipo do dado informado é number.";
+document.addEventListener("DOMContentLoaded",function() {
+
+    document.getElementById("formulario").addEventListener("submit",verificarTipo);
+
+    function verificarTipo(evento) {
+        evento.preventDefault(); // prevente da pagina recarregar apos enviar formulario
+        let dado = document.getElementById("input").value.trim();
+        let tipoDado;
+
+        // numero
+        if (!isNaN(dado) && dado !== '') { // converte a string em numero
+            tipoDado = 'number';
+        }
+        // booleano
+        else if (dado.toLowerCase() === "true" || dado.toLowerCase() === "false") { // toLowerCase() previne caso o usuario digite maiusculo
+            tipoDado = 'boolean';
+        }
+        // string
+        else {
+            tipoDado = 'string';
+        }
+
+        if(confirm("Deseja verificar o tipo do dado informado?")) {
+            document.getElementById("resultado").innerHTML = "O tipo do dado informado é "+ tipoDado +".";
+        }
+        else {
+            document.getElementById("resultado").innerHTML = "Obrigado por visitar esta página.";
+        }
     }
-    // booleano
-    else if (dado == "true" || dado == "false") {
-        document.getElementById("resultado").innerHTML = "O tipo do dado informado é de boolean.";
-    }
-    // string
-    else {
-        document.getElementById("resultado").innerHTML = "O tipo do dado informado é string.";
-    }
-}
-else {
-    document.getElementById("resultado").innerHTML = "Obrigado por visitar esta página.";
-}
+});
--- semana2/script3.js
+++ semana2/script3.js
@@ -1,7 +1,20 @@
-function ePalindromo() {
-    let string = document.getElementById("input").value;
-    const contador = [];
-    for (i = 0; i < string.length; i++) {
-        let stringReversa = contador[string.length];
-    }
+let string = prompt("Digite uma palavra ou uma frase qualquer: ");
+
+function ePalindromo(string) {
+    const stringAlterada = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase(); // remove caracteres que nao sao letras e converte pra minusculo
+    const stringReversa = stringAlterada.split('').reverse().join('');
+    document.write = stringAlterada;
+    // split('') - separa a string em um vetor de caracteres, '' indica que deve ser feito por cada caractere
+    // reverse() - inverte a ordem do vetor de caracteres
+    // join('') - junta o vetor de caracteres em uma string novamente, nesse caso, invertida.    
+
+    return stringAlterada === stringReversa;
 }
+
+if(ePalindromo(string)) {
+    alert(string + " é um palíndromo.")
+}
+else {
+    alert(string +" não é um palíndromo.")
+}
+
