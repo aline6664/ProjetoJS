d255b5b4657ffa83c97f92dd5d2f27a71c4beb51
aline6664 <gley.line@gmail.com>
2025-03-11 11:38:50 -0300

    UPDATE semana 3, ADD semana 4

--- semana3/script2.js
+++ semana3/script2.js
@@ -6,32 +6,68 @@ let modelo = document.getElementById("inputModelo").value;
 
 // classe
 function Carro() {
-    var marca;
+    // marca
     this.setMarca = function(Marca) {
         this.marca = Marca;
     }
     this.getMarca = function() {
         return this.marca;
     }
-    var modelo;
+    // modelo
     this.setModelo = function(Modelo) {
         this.modelo = Modelo;
     }
     this.getModelo = function() {
         return this.modelo;
     }
+    // ano
+    this.setAno = function(ano) {
+        this.ano = ano;
+    }
+    this.getAno = function() {
+        return this.ano;
+    }
+    // cor
+    this.setCor = function(Cor) {
+        this.cor = Cor;
+    }
+    this.getCor = function() {
+        return this.cor;
+    }
+    // kilometragem
+    this.setKilom = function(Kilometragem) {
+        this.kilometragem = Kilometragem;
+    }
+    this.getKilom = function() {
+        return this.kilometragem;
+    }
+    // valor_fipe
+    this.setFipe = function(Valor_fipe) {
+        this.valor_fipe = Valor_fipe;
+    }
+    this.getFipe = function() {
+        return this.valor_fipe;
+    }
 }
 // marca, modelo, ano, cor, kilometragem, valor_fipe
 
 var umCarro = new Carro();
 umCarro.setMarca(marca);
-umCarro.setModelo(modelo)
-document.getElementById("resultado").innerHTML = "Marca: "+ umCarro.getMarca() +" Modelo: "+ umCarro.getModelo(); 
+umCarro.setModelo(modelo);
+umCarro.setAno(ano);
+umCarro.setCor(cor);
+umCarro.setKilom(kilometragem);
+umCarro.setFipe(valor_fipe);
 
-/* function anosUtilizacao() {
+document.getElementById("resultado").innerHTML = 
+"Marca: "+ umCarro.getMarca() +"<br> Modelo: "+ umCarro.getModelo() +
+"<br> Ano: "+ umCarro.getAno() +"<br> Cor: "+ umCarro.getCor() +
+"<br> Kilometragem: "+ umCarro.getKilom() +"<br> Valor Fipe: "+ umCarro.getFipe();
+
+function anosUtilizacao() {
 
 }
 
 function valorMercado() {
 
-}*/
+}
